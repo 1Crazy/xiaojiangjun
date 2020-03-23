24c5d59dfa8ab4934ab1cbaecf5d237cad036d32
kyle <wangyuhong7777@163.com>
2020-03-23 18:11:22 +0800

    部分完成

--- xiangjiangjun/pages/isSureOrder/isSureOrder.vue
+++ xiangjiangjun/pages/isSureOrder/isSureOrder.vue
@@ -1,14 +1,27 @@
 <template>
 	<view>
+		<view class="header">
+			<view class="h-title">收货方式</view>
+			<uni-list>
+			    <uni-list-item title="标题文字" thumb=""></uni-list-item>
+			</uni-list>
+		</view>
 		
 	</view>
 </template>
 
 <script>
+	import uniList from "@/components/uni/uni-list/uni-list.vue"
+	import uniListItem from "@/components/uni/uni-list-item/uni-list-item.vue"
+	import './index.scss'
 	export default {
+		components: {
+			uniList,
+			uniListItem
+		},
 		data() {
 			return {
-				
+				imgSrc: this.$store.state.imgSrc,
 			};
 		}
 	}
