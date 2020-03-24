ab8040f1b83f229d1b9d6073a5ba2a085a97c5c3
kyle <wangyuhong7777@163.com>
2020-03-24 17:08:02 +0800

    附近门店

---
+++ xiangjiangjun/pages/chooiseStoreName/chooiseStoreName.vue
@@ -0,0 +1,47 @@
+<template>
+	<view>
+		<view class="headerWrap">
+			<view class="header">
+				<input class="ipt" type="text" value="" placeholder="搜索您要找的门店名称" />
+				<image :src="imgSrc+'public/fdj1.png'" class="img"></image>
+			</view>
+		</view>
+		<view class="listWrap">
+			<view>
+				<view class="distance">1.5km</view>
+				<view class="storeName">成都市高新区环球店</view>
+			</view>
+			<button type="default" class="btn">确定</button>
+		</view>
+		<view class="listWrap">
+			<view>
+				<view class="distance">1.5km</view>
+				<view class="storeName">成都市高新区环球店</view>
+			</view>
+			<button type="default" class="btn">确定</button>
+		</view>
+		<view class="listWrap">
+			<view>
+				<view class="distance">1.5km</view>
+				<view class="storeName">成都市高新区环球店</view>
+			</view>
+			<button type="default" class="btn">确定</button>
+		</view>
+		<button type="default" class="lookMore">查看更多</button>
+	</view>
+</template>
+
+<script>
+	import './index.scss'
+	export default {
+		data() {
+			return {
+				imgSrc: this.$store.state.imgSrc,
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
