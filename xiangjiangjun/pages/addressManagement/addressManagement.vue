e4cb9dd00a0a215d05755e9b5b7dd0656f3ce6d2
wangchengcheng <wangchengcheng>
2020-03-26 18:18:21 +0800

    地址选择

--- xiangjiangjun/pages/addressManagement/addressManagement.vue
+++ xiangjiangjun/pages/addressManagement/addressManagement.vue
@@ -1,6 +1,7 @@
 <template>
 	<view>
 		<view class="addrssListWrap">
+		 <radio-group>
 			<view class="addressList" v-for="(item ,index) in lists" :key="index">
 				<view class="top">
 					<view>
@@ -9,16 +10,17 @@
 					</view>
 					<image class="img" :src="imgSrc+'public/dizhi_xiugai.png'" @tap="gotoAddOrUpdateAddress()"></image>
 				</view>
-				<view class="bottom">
-					<label class="radio">
-						<radio value="x" /><text class="defaultAddress" style="margin-left: 18rpx;">默认地址</text>
-					</label>
-					<view class="del">
-						<view>删除</view>
-						<image class="img" :src="imgSrc+'public/delete.png'"></image>
+					<view class="bottom">
+							<label class="radio" @tap="setDefault(item.id)">
+								<radio :value="item.id" :checked="item.isdefault==0?false:true"/><text class="defaultAddress" style="margin-left: 18rpx;" >默认地址</text>
+							</label>
+						<view class="del">
+							<view @tap="delAddress(item.id)">删除</view>
+							<image class="img" :src="imgSrc+'public/delete.png'"></image>
+						</view>
 					</view>
-				</view>
 			</view>
+		</radio-group>
 		</view>
 		<view class="btnWrap">
 			<button class="addAdressBtn" @tap="gotoAddOrUpdateAddress()">+ 添加地址</button>
@@ -46,6 +48,7 @@
 			// 
 			getData(){
 				const that = this;
+				
 				Request(
 					'member.address.get_list'
 				).then((res)=>{
@@ -60,6 +63,37 @@
 			gotoAddOrUpdateAddress() {
 				uni.navigateTo({
 					url: '/pages/addOrUpdateAddress/addOrUpdateAddress'
+				})
+			},
+			delAddress(id){
+				Request(
+					'member.address.delete',
+					{
+						id:id
+					}
+				).then((res)=>{
+					console.log(res)
+					// 成功方法
+					this.getData()
+				})
+				.catch((res)=>{
+					// 失败方法
+				})
+			},
+			setDefault(id){
+				Request(
+					'member.address.set_default',
+					{
+						id:id
+					}
+				).then((res)=>{
+					console.log(res)
+					
+					// 成功方法
+					// this.getData()
+				})
+				.catch((res)=>{
+					// 失败方法
 				})
 			}
 		}
