41758be63ac508f8f5e2cc1a86f97f1999fc6750
kyle <wangyuhong7777@163.com>
2020-03-31 10:25:16 +0800

    体验者申请成功页面完成

--- xiangjiangjun/pages/experiencer/experiencer.vue
+++ xiangjiangjun/pages/experiencer/experiencer.vue
@@ -64,18 +64,41 @@
 		</view>
 		<view class="qualifyWrap" v-if="isQualify">
 			<view class="nav">
-				<view class="word active">
+				<view :class="isQualify && currentTab==1 ?  'word active' : 'word'" @tap="cutTab(1)">
 					核销
 					<view class="line"></view>
 				</view>
-				<view class="wrod">
+				<view :class="isQualify && currentTab==2 ?  'word active' : 'word'" @tap="cutTab(2)">
 					体验记录
 					<view class="line"></view>
 				</view>
 			</view>
-			<view class="qrcodeWrap">
+			<view class="qrcodeWrap" v-if="isQualify && currentTab == 1">
 				<image src="../../static/public/adress1.png" class="img"></image>
 				<view class="word">商家扫码核销</view>
+			</view>
+			<view class="qrcodeWrap" v-if="isQualify && currentTab == 2">
+				<view class="itemWrap">
+					<view class="title">XX汽车美容服务</view>
+					<view class="textcontent">
+						<view class="txt1">汽车贴膜</view>
+						<view class="txt2">2019-12-30</view>
+					</view>
+				</view>
+				<view class="itemWrap">
+					<view class="title">XX汽车美容服务</view>
+					<view class="textcontent">
+						<view class="txt1">汽车贴膜</view>
+						<view class="txt2">2019-12-30</view>
+					</view>
+				</view>
+				<view class="itemWrap">
+					<view class="title">XX汽车美容服务</view>
+					<view class="textcontent">
+						<view class="txt1">汽车贴膜</view>
+						<view class="txt2">2019-12-30</view>
+					</view>
+				</view>
 			</view>
 		</view>
 	</view>
@@ -87,8 +110,14 @@
 		data() {
 			return {
 				imgSrc: this.$store.state.imgSrc,
-				isQualify: true
+				isQualify: true,
+				currentTab: 2
 			};
+		},
+		methods:{
+			cutTab(num){
+				this.currentTab = num
+			},
 		}
 	}
 </script>
