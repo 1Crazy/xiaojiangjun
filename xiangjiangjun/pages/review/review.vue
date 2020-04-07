c1c281d942f4233cf511afb7f7cb1a7db0c6f955
kyle <wangyuhong7777@163.com>
2020-04-07 18:57:47 +0800

    订单评论部分

--- xiangjiangjun/pages/review/review.vue
+++ xiangjiangjun/pages/review/review.vue
@@ -1,16 +1,12 @@
 <template>
 	<view>
 		<view class="header">
-			<product-title img="../../static/public/goumai_guanbi.png" title='YBM/意奔玛空调滤清YMB3140007空调滤芯空调滤芯' shortTitle='我是短标题' borderBottomStyle='none'></product-title>
+			<product-title v-for="(item,index) in goods" :key="index" :img="item.thumb" :title='item.title' shortTitle='我是短标题' borderBottomStyle='none'></product-title>
 		</view>
 		<view class="content">
 			<view class="ct-header">
 				<view class="title">星级评价：</view>
-				<image :src="imgSrc+'productInfo/star.png'" mode="" class="star"></image>
-				<image :src="imgSrc+'productInfo/star.png'" mode="" class="star"></image>
-				<image :src="imgSrc+'productInfo/star.png'" mode="" class="star"></image>
-				<image :src="imgSrc+'productInfo/star.png'" mode="" class="star"></image>
-				<image :src="imgSrc+'productInfo/star.png'" mode="" class="star"></image>
+				<image v-for="(item , index) in starArray" :key="index" :src="imgSrc+'public/'+item" class="star" @tap="changeStar(index)"></image>
 			</view>
 			<view class="txtareaWrap">
 				<image src="../../static/public/pen.png" class="img"></image>
@@ -22,7 +18,7 @@
 				<image class="img" src="../../static/public/index-S.png" mode=""></image>
 				<image class="img" src="../../static/public/index-S.png" mode=""></image>
 				<!-- 图片以上是循环，图片以下是上传按钮的图片 -->
-				<image class="img" src="../../static/public/addpic.png" mode=""></image>
+				<image class="img" src="../../static/public/addpic.png" @tap="uploadImage" mode=""></image>
 			</view>
 		</view>
 		<button class="wordBtn">发表评论</button>
@@ -39,6 +35,9 @@
 		data() {
 			return {
 				imgSrc: this.$store.state.imgSrc,
+				goods:[],
+				starArray: ['star_n.png','star_n.png','star_n.png','star_n.png','star_n.png'],
+				uploadImg: [],
 			};
 		},
 		onLoad(e) {
@@ -53,15 +52,54 @@
 				Request(
 					'order.comment',
 					{
-						id:id
+						id
 					}
 				)
 				.then((res)=>{
-					
+					console.log(res,'res')
+					this.goods = res.data.goods
 				})
 				.catch((res)=>{
 					
 				})
+			},
+			changeStar(idx){
+				this.starArray = this.starArray.map((curr,index)=>{
+					idx>=index ? curr = 'star.png' : curr = 'star_n.png';
+					return curr
+				})
+			},
+			uploadImage(){
+				console.log(3333)
+				const that  = this
+				if (that.uploadImg.length >= 6) {
+				  uni.showToast({
+					title:'图片最多上传6张',
+					icon: 'none'
+				  })
+				  return false
+				}
+				uni.chooseImage({
+				    count: 6, //默认9
+				    sizeType: ['original', 'compressed'], //可以指定是原图还是压缩图，默认二者都有
+				    sourceType: ['album'], //从相册选择
+				    success: function (res) {
+						// console.log(res,'resresres')
+				  //       console.log(JSON.stringify(res.tempFilePaths));
+						that.uploadImg.push(res.tempFilePaths)
+						uni.uploadFile({
+						    url: 'util.uploader.upload&file=file', //仅为示例，非真实的接口地址
+						    filePath: JSON.stringify(that.uploadImg),
+						    name: 'file',
+						    formData: {
+						        'user': 'test'
+						    },
+						    success: (uploadFileRes) => {
+						        // console.log(uploadFileRes.data);
+						    }
+						});
+				    }
+				});
 			}
 		}
 	}
